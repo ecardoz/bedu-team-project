4a0e7dac7da8560afcab59a7010e06d5a1f8fbfa
Hiram Chavez <virtual.liga@gmail.com>
2023-03-13 11:52:03 -0600

    Update Utils.java

--- Postwork5/src/main/java/com/bedu/java/backend/basico/Postwork5/Utils.java
+++ Postwork5/src/main/java/com/bedu/java/backend/basico/Postwork5/Utils.java
@@ -1,2 +1,30 @@
-package com.bedu.java.backend.basico.Postwork5;public class utils {
+package com.bedu.java.backend.basico.Postwork5;
+
+public class Utils {
+
+    //Solo debe contener caracteres válidos: números, guion medio y espacio en blanco
+    public static boolean isPhoneWithCharactersValid(String telefono) {
+        return telefono.matches("[^a-zA-Z]+\s*-*");
+    }
+
+    //Solo debe contener 10 números
+    public static boolean isPhoneWithTenNumbers(String telefono) {
+        String onlyNumbers = getOnlyNumbers(telefono);
+        return onlyNumbers.matches("[0-9]{10}");
+    }
+
+    //Formateando el telefono: (55)-5555-5555
+    public static String formatPhone(String telefono) {
+        String onlyNumbers = getOnlyNumbers(telefono);
+        String firstPart = onlyNumbers.substring(0, 2);
+        String secondPart = onlyNumbers.substring(2, 6);
+        String thirdPart = onlyNumbers.substring(6, 10);
+        return "(" + firstPart + ")" + "-" + secondPart + "-" + thirdPart;
+    }
+
+    //Dejando solo los numeros
+    private static String getOnlyNumbers(String telefono) {
+        return telefono.replaceAll("[^0-9]", "");
+    }
+
 }
