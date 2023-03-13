351d185197ce5dd33af9d6b9a419af288c02a4fe
Hiram Chavez <virtual.liga@gmail.com>
2023-03-13 11:54:20 -0600

    Update Persona.java

--- Postwork5/src/main/java/com/bedu/java/backend/basico/Postwork5/model/Persona.java
+++ Postwork5/src/main/java/com/bedu/java/backend/basico/Postwork5/model/Persona.java
@@ -1,2 +1,25 @@
-package com.bedu.java.backend.basico.Postwork5.model;public class Persona {
+package com.bedu.java.backend.basico.Postwork5.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Persona {
+    private String nombre;
+    private Telefono telefono;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Telefono getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Telefono telefono) {
+        this.telefono = telefono;
+    }
 }
