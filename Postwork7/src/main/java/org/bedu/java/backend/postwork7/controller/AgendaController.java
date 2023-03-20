43abb5ffbbd3fa5d51851597131b07921476a4de
AlmaLst <alma.loverasotelo@gmail.com>
2023-03-19 22:05:38 -0600

    [Update] agenda controller and added index and contactList html files

---
+++ Postwork7/src/main/java/org/bedu/java/backend/postwork7/controller/AgendaController.java
@@ -0,0 +1,48 @@
+package org.bedu.java.backend.postwork7.controller;
+
+import jakarta.validation.Valid;
+import org.bedu.java.backend.postwork7.model.Persona;
+import org.bedu.java.backend.postwork7.services.AgendaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AgendaController {
+    private final AgendaService agendaService;
+
+
+    @Autowired
+    public AgendaController(AgendaService agendaService) {
+
+        this.agendaService = agendaService;
+    }
+
+    @GetMapping({"/","/index"})
+    public String formularioRegistro (Model model){
+        model.addAttribute("persona", new Persona());
+        return "index";
+    }
+
+    @PostMapping("/registro")
+    public ModelAndView registra(@Valid Persona persona, Errors errors){
+        agendaService.guardaPersona(persona);
+        String vistaResultado = "listaContactos";
+        String mensajeConfirmacion = "Contacto guardado correctamente";
+
+        if(errors.hasErrors()){
+            vistaResultado = "index";
+            mensajeConfirmacion = "Contacto no guardado";
+        }
+
+        ModelAndView mav = new ModelAndView(vistaResultado);
+        mav.addObject("listaPersonas", agendaService.getPersonas());
+        if(!mensajeConfirmacion.isEmpty()){
+            mav.addObject("mensajeConfirmacion", mensajeConfirmacion);
+        }
+        return mav;
+    }
