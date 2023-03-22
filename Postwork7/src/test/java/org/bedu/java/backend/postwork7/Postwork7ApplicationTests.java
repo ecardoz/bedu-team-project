0f67b4130ca00570ad21efe0f75f6a7bce8f3e93
Daniel Maldonado <daniel_dlcmaldonado@hotmail.com>
2023-03-21 19:18:21 -0600

    Postwork 7 thymeleaf con Springboot

---
+++ Postwork7/src/test/java/org/bedu/java/backend/postwork7/Postwork7ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedu.java.backend.postwork7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Postwork7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
